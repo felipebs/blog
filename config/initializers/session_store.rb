1a08c094cb5fdcb43fa7cc326ef6244f5fa80dc8
Felipe Barbosa Silveira <febsilveira@hotmail.com>
2010-02-20 17:30:22 -0200

    Adding prject to Github

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_blog_session',
+  :secret      => 'b945f6617089cea553f86680eaf339078d93167839fa51f0ae7297f5fe7aead7db241b5a9b79b9701a8567f0dab8339397c09415db134f6ec192da3cea03d8a2'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
