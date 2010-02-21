fd79244b4a4a221cd86ea1fb45fd5ad8f1703f2e
Felipe Barbosa Silveira <febsilveira@hotmail.com>
2010-02-21 03:41:51 -0300

    Adding partials

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,6 @@
 class Comment < ActiveRecord::Base
+  belongs_to :post
+
+  validates_presence_of :author, :message => " - deve se identificar"
+  validates_presence_of :comment, :message => " - deve escrever o comentário"
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  has_many :comments
+
+  validates_presence_of :title, :message => " - deve ser preenchido"
+  validates_presence_of :post, :message => " - deve ser escrito"
+  validates_presence_of :author, :message => " - deve se identificar"  
 end
